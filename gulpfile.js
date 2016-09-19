3b0b2e85c1a00b8f3ea4a3e2a653d64ab9e67050
Abhishek Raj <abhishekrajinfo@gmail.com>
2016-09-19 10:56:30 +0000

    updated

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require("gulp"),
+    // For livereloading]
+    sync = require("browser-sync"),
+    // To compile sass file
+    sass = require("gulp-sass"),
+    // To compress images
+    imagemin = require("gulp-imagemin"),
+    // To handle css errors
+    plumber = require("gulp-plumber"),
+    // For cross browser compilation
+    autoprefixer = require("gulp-autoprefixer");
+    
+    
+// Task style
+// Compile
+gulp.task('style', function(){
+    gulp.src('scss/*.scss')
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(plumber())
+        .pipe(gulp.dest('css/'));
+});
+
+// Task image
+// compress
+gulp.task('image', function(){
+    gulp.src('img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('images'));
+});
+
+// Task Reload
+// Livereloading
+gulp.task('reload', function(){
+    sync({
+       files: '*.php, *.html, scss/*.scss', 
+       port: 8082
+    });
+});
+
+// Task Watch
+// Watch changes in files for compilation and reloading
+gulp.task('watch', function(){
+   gulp.watch('scss/*.scss',['style'])
+   gulp.watch('img/*',['image'])
+});
+
+// Task Default
+// To run all task using default gulp commmand
+gulp.task('default',['style','image','watch', 'reload']);
